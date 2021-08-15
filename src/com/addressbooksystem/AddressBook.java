2a109fe6a52bc99af64f166182d93d38b7f4c7e9
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-15 22:59:12 +0530

    [Devesh]: Added ability to edit the contact in Address Book program

--- src/com/addressbooksystem/AddressBook.java
+++ src/com/addressbooksystem/AddressBook.java
@@ -30,9 +30,13 @@ class ContactBook{
 	
 }
 
+
 public class AddressBook {
-	public static void main(String[] args) {
-		ArrayList<ContactBook> arr= new ArrayList();
+	
+	public static ArrayList<ContactBook> arr= new ArrayList();
+	
+	//method for adding contact
+	public static void addcontact() {
 		
 		//taking input
 		Scanner sc = new Scanner(System.in);
@@ -58,11 +62,133 @@ public class AddressBook {
 			
 			String phonenumber=sc.nextLine(); 
 			
+			//Below substring done due to ide memory buffer error
+			firstname = firstname.replaceAll("\\s", "");
+			phonenumber = phonenumber.replaceAll("\\s", "");
+			
 			
 			//creation of object or address book creation
 			ContactBook cb = new ContactBook(firstname,lastname,address,state,city,emailid,zipc,phonenumber);
 			arr.add(cb);
 			record--;
 		}
+	}
+	
+	//method for editing contact
+	public static void editcontact() {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter records to be edited: ");
+		int editrecord = sc.nextInt();
+		
+		while ( editrecord!= 0 ) {
+			System.out.println("Enter firstname lastname address state city emailid zipc phonenumber");
+			String input1 = sc.nextLine();
+			input1 = input1.replaceAll("\\s", "");
+			if ( input1.equals("firstname") ) {
+				String name = sc.nextLine();
+				String updatename = sc.nextLine();
+				
+				int i;
+				for(i = 0 ; i < arr.size(); i++) {
+					if (arr.get(i).firstname.equals(name) ) {
+						arr.get(i).firstname=updatename;
+						break;
+					}
+				}
+			}
+			else if ( input1.equals("lastname") ) {
+				String name = sc.nextLine();
+				String updatename = sc.nextLine();
+				
+				int i;
+				for(i = 0 ; i < arr.size(); i++) {
+					if (arr.get(i).lastname.equals(name) ) {
+						arr.get(i).lastname=updatename;
+						break;
+					}
+				}
+			}
+			else if ( input1.equals("address") ) {
+				String name = sc.nextLine();
+				String updatename = sc.nextLine();
+				
+				int i;
+				for(i = 0 ; i < arr.size(); i++) {
+					if (arr.get(i).address.equals(name) ) {
+						arr.get(i).address=updatename;
+						break;
+					}
+				}
+			}
+			else if ( input1.equals("state") ) {
+				String name = sc.nextLine();
+				String updatename = sc.nextLine();
+				
+				int i;
+				for(i = 0 ; i < arr.size(); i++) {
+					if (arr.get(i).state.equals(name) ) {
+						arr.get(i).state=updatename;
+						break;
+					}
+				}
+			}
+			else if ( input1.equals("city") ) {
+				String name = sc.nextLine();
+				String updatename = sc.nextLine();
+				
+				int i;
+				for(i = 0 ; i < arr.size(); i++) {
+					if (arr.get(i).city.equals(name) ) {
+						arr.get(i).city=updatename;
+						break;
+					}
+				}
+			}
+			else if ( input1.equals("emailid") ) {
+				String name = sc.nextLine();
+				String updatename = sc.nextLine();
+				
+				int i;
+				for(i = 0 ; i < arr.size(); i++) {
+					if (arr.get(i).emailid.equals(name) ) {
+						arr.get(i).emailid=updatename;
+						break;
+					}
+				}
+			}
+			else if ( input1.equals("zipc") ) {
+				int name = sc.nextInt();
+				int updatename = sc.nextInt();
+				
+				int i;
+				for(i = 0 ; i < arr.size(); i++) {
+					if (arr.get(i).zipc == name ) {
+						arr.get(i).zipc=updatename;
+						break;
+					}
+				}
+			}
+			else if ( input1.equals("phonenumber") ) {
+				String name = sc.nextLine();
+				String updatename = sc.nextLine();
+				
+				int i;
+				for(i = 0 ; i < arr.size(); i++) {
+					if (arr.get(i).phonenumber.equals(name) ) {
+						arr.get(i).phonenumber=updatename;
+						break;
+					}
+				}
+			}
+			editrecord--;
+		}
+	}
+	
+	//main method
+	public static void main(String[] args) {
+		
+		addcontact();
+		editcontact();
 	}
 }
