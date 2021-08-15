568126af491eeaf0cbf376c8d9ff401438ccc021
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-15 21:49:20 +0530

    [Devesh]: Added ability add contact in created contact book

--- src/com/addressbooksystem/AddressBook.java
+++ src/com/addressbooksystem/AddressBook.java
@@ -12,11 +12,11 @@ class ContactBook{
 	String city; 
 	String emailid;
 	int zipc; 
-	int phonenumber; 
+	String phonenumber; 
 	
 	
 	//constructor of class
-	ContactBook( String firstname, String lastname, String address, String state, String city, String emailid, int zipc, int phonenumber){
+	ContactBook( String firstname, String lastname, String address, String state, String city, String emailid, int zipc, String phonenumber){
 		this.firstname = firstname; 
 		this.lastname=lastname; 
 		this.address=address; 
@@ -32,7 +32,37 @@ class ContactBook{
 
 public class AddressBook {
 	public static void main(String[] args) {
+		ArrayList<ContactBook> arr= new ArrayList();
 		
+		//taking input
+		Scanner sc = new Scanner(System.in);
 		
+		//Adding records.
+		System.out.println("Enter number of record:");
+		int record = sc.nextInt();
+		while ( record!=0 ) {
+			
+			String firstname= sc.nextLine();
+			
+			String lastname= sc.nextLine();
+			
+			String address= sc.nextLine();
+			
+			String state= sc.nextLine();
+			
+			String city= sc.nextLine();
+			
+			String emailid= sc.nextLine();
+			
+			int zipc=sc.nextInt(); 
+			
+			String phonenumber=sc.nextLine(); 
+			
+			
+			//creation of object or address book creation
+			ContactBook cb = new ContactBook(firstname,lastname,address,state,city,emailid,zipc,phonenumber);
+			arr.add(cb);
+			record--;
+		}
 	}
 }
